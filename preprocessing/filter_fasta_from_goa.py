1f863c49905b4aa169d666d7dfd30da7fb1db65c
Brian Osborne <bosborne@alum.mit.edu>
2023-12-30 22:18:46 -0500

    Remove unused script

--- preprocessing/filter_fasta_from_goa.py
+++ preprocessing/filter_fasta_from_goa.py
@@ -16,8 +16,8 @@ def main():
     Example:
 
     python preprocessing-scripts/filter_fasta_from_goa.py \
-    data/goa/t0/goa_uniprot_all_noiea.gaf \
-    ~/gargantua/trembl/uniprot_trembl.fasta \
+    data/goa_uniprot_all_noiea.gaf \
+    trembl/uniprot_trembl.fasta \
     data/trembl/trembl_seqs_found_in_goa_2021_10_26.fasta
     """
     logging.basicConfig(level=logging.INFO)
@@ -26,9 +26,7 @@ def main():
     parser.add_argument(
         "goa", help="Path to Gene Ontology Annotation (.gaf file)"
     )
-    parser.add_argument(
-        "trembl", help="Path to TrEMBL DB (.fasta file)"
-    )
+    parser.add_argument("trembl", help="Path to TrEMBL DB (.fasta file)")
     parser.add_argument("output", help="Output file")
     args = parser.parse_args()
 
@@ -56,9 +54,7 @@ def main():
             accession = record.id.split("|")[1]
             if accession in protein_ids:
                 records_saved += 1
-                logging.info(
-                    f"Saving record {records_saved}: {record.id}"
-                )
+                logging.info(f"Saving record {records_saved}: {record.id}")
                 saved_records.append(record)
 
     logging.info(f"Found {len(saved_records)} matching sequences")
