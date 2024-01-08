c5b1a43c6bac80be4d019f433e6991d5686c0bdd
Brian Osborne <bosborne@c100-002.frontera.tacc.utexas.edu>
2024-01-08 12:31:01 -0600

    Slurm script in progress

--- utility-scripts/repeat_slurm_job.py
+++ utility-scripts/repeat_slurm_job.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
 
     a = pyslurm.job()
     pending = a.find("job_state", "PENDING")
-    # running = a.find("job_state", "RUNNING")
+    myjobs = a.find("account", args.account)
     # jobs = a.get()
