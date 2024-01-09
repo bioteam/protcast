28ec8ff5bc5cf2546fea29e11413e52964d2d361
Brian Osborne <bosborne@alum.mit.edu>
2024-01-08 21:29:48 -0500

    Slurm script in progress

--- utility-scripts/repeat_slurm_job.py
+++ utility-scripts/repeat_slurm_job.py
@@ -28,4 +28,20 @@ if __name__ == "__main__":
     a = pyslurm.job()
     pending = a.find("job_state", "PENDING")
     myjobs = a.find("account", args.account)
-    # jobs = a.get()
+
+    # test_job = {
+    #     "wrap": "sleep 3600",
+    #     "job_name": "pyslurm_test_job",
+    #     "ntasks": 2,
+    #     "cpus_per_task": 3,
+    #     "partition": "abc",
+    #     "script": "xyz",
+    #     "nodes": 3,
+    #     "time": "1:00:00",
+    #     "output": "a.out",
+    #     "error": "a.err"
+    # }
+    # test_job_id = pyslurm.job().submit_batch_job(test_job)
+    # test_job_search = pyslurm.job().find(name="name", val=test_job["job_name"])
+    # test_job_info = pyslurm.job().find_id(test_job_id)
+    # rc = pyslurm.slurm_kill_job(test_job_id, Signal=9, BatchFlag=pyslurm.KILL_JOB_BATCH)
