4c1b8a241c0c997ece70564a3e0ef652a1ad6953
Brian Osborne <bosborne@alum.mit.edu>
2024-02-02 10:58:50 -0500

    Minor edits to Slurm scripts

--- utility-scripts/create_simple_dataset.sh
+++ utility-scripts/create_simple_dataset.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name create_simple_dataset
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=briano@bioteam.net
-#SBATCH -o /scratch1/04769/bosborne/create_simple_dataset.out
-#SBATCH -e /scratch1/04769/bosborne/create_simple_dataset.err
+#SBATCH -o create_simple_dataset.out
+#SBATCH -e create_simple_dataset.err
 #SBATCH -N 1
 #SBATCH -n 20
 
@@ -12,8 +12,8 @@ DATE=2024-01-17
 
 cd $SCRATCH
 python3 $HOME/git/ProtCast/preprocessing/create_simple_dataset.py \
--o $DATE/GO/go.obo \
--g $DATE/GO/filtered_goa_uniprot_all_noiea.gaf \
--t $DATE/UniProt/uniprot_trembl.fasta \
--s $DATE/UniProt/uniprot_sprot.dat \
--O 2-1-2024
+    -o $DATE/GO/go.obo \
+    -g $DATE/GO/filtered_goa_uniprot_all_noiea.gaf \
+    -t $DATE/UniProt/uniprot_trembl.fasta \
+    -s $DATE/UniProt/uniprot_sprot.dat \
+    -O 2-1-2024
