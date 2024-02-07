bfde0aae51139dcebf5bc09de9d029a5b811d6b4
Brian Osborne <bosborne@alum.mit.edu>
2024-02-07 17:54:13 -0500

    Update utility scripts

--- utility-scripts/create_simple_dataset.sh
+++ utility-scripts/create_simple_dataset.sh
@@ -9,6 +9,7 @@
 #SBATCH -n 20
 
 DATE=2024-01-17
+OUTPUT_DIR=$(date +%m-%d-%Y -d now)
 
 cd $SCRATCH
 python3 $HOME/git/ProtCast/utility-scripts/create_simple_dataset.py \
@@ -16,4 +17,6 @@ python3 $HOME/git/ProtCast/utility-scripts/create_simple_dataset.py \
     -g $DATE/GO/filtered_goa_uniprot_all_noiea.gaf \
     -t $DATE/UniProt/uniprot_trembl.fasta \
     -s $DATE/UniProt/uniprot_sprot.dat \
-    -O $(date +%m-%d-%Y -d now)
+    -O $OUTPUT_DIR
+python3 $HOME/git/ProtCast/utility-scripts/create_dataset_stats.py \
+    -i $OUTPUT_DIR/SimpleDataset.bin
