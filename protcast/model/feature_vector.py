5464160fb531047358a10f1900233b6de674a99d
Brian Osborne <bosborne@alum.mit.edu>
2024-02-14 09:21:10 -0500

    Create module for feature vector creaation

---
+++ protcast/model/feature_vector.py
@@ -0,0 +1,37 @@
+import os
+import tempfile
+from ifeatpro.features import get_feature
+from typeguard import typechecked
+
+
+@typechecked
+def get_ifeatpro_features(
+    feature: str, seqs: dict
+) -> list[list[float]]:
+    """get_ifeatpro_features
+    Returns a list of arrays or "feature vectors" using ifeatpro
+
+    Parameters
+    ----------
+    feature: str
+        Feature name, e.g. 'ctriad'
+    seqs: dict
+        Key is protein id, value is protein sequence
+   
+    Returns
+    -------
+    List of lists of floats
+    """
+    tmpdir = tempfile.TemporaryDirectory()
+    tmpfasta = tempfile.NamedTemporaryFile()
+    # Write fasta file
+    with open(tmpfasta.name, "w") as f:
+        for pid, seq in seqs.items:
+            f.write(">" + pid + "\n" + seq + "\n")
+
+    get_feature(tmpfasta.name, feature, tmpdir.name)
+    with open(os.path.join(tmpdir.name, feature + ".csv")) as f:
+        features = [
+            [float(x) for x in line.split(",")[1:]] for line in f.readlines()
+        ]
+    return features
--- protcast/model/utils.py
+++ protcast/model/utils.py
@@ -1,43 +1,5 @@
-import os
-import tempfile
-from ifeatpro.features import get_feature
 import numpy as np
 from typeguard import typechecked
-
-
-@typechecked
-def get_protein_feature(
-    feature: str, seqs: list[str], pids: list[str]
-) -> list[list[float]]:
-    """get_protein_feature
-    Returns a list of arrays or "feature vectors" using ifeatpro
-
-    Parameters
-    ----------
-    feature: str
-        Feature name, e.g. 'ctriad'
-    seqs: list
-        List of sequence strings
-    pids: list
-        List of Swisssprot accessions, e.g. 'ARAB_ABC5'
-
-    Returns
-    -------
-    List of lists of floats
-    """
-    tmpdir = tempfile.TemporaryDirectory()
-    tmpfasta = tempfile.NamedTemporaryFile()
-    # Write fasta file
-    with open(tmpfasta.name, "w") as f:
-        for pid, seq in zip(pids, seqs):
-            f.write(">" + pid + "\n" + seq + "\n")
-
-    get_feature(tmpfasta.name, feature, tmpdir.name)
-    with open(os.path.join(tmpdir.name, feature + ".csv")) as f:
-        features = [
-            [float(x) for x in line.split(",")[1:]] for line in f.readlines()
-        ]
-    return features
 
 
 @typechecked
