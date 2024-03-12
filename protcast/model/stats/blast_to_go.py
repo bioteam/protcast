a48c069d505e545ff8e2a0f3d76ff208eb766216
Brian Osborne <bosborne@alum.mit.edu>
2024-03-12 12:50:30 -0400

    Move code from test to class, tests pass

--- protcast/model/stats/blast_to_go.py
+++ protcast/model/stats/blast_to_go.py
@@ -1,27 +1,38 @@
+import json
+import logging
+import os
+import requests
 from typeguard import typechecked
 from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
-
+from Bio import Entrez
 
 @typechecked
 class BlastToGo:
     """BlastToGo
-    This function takes a protein sequence, database name,
-    e-value threshold, and minimum identity as input and performs a protein blast search
+    This clast takes a protein sequence and performs a protein Blast search
     against a NCBI database, returning proteins with at least min_identity percent similarity.
 
     Attributes
     ----------
-    protein_seq (str): Path to the protein FASTA file.
-    database (str): Name of the NCBI protein database (e.g., "nr").
-    e_value (float): E-value threshold for significant alignments.
-    min_identity (int): Minimum percent sequence identity for hits.
+    protein_seq (str): 
+        Path to the protein FASTA file.
+    database (str): 
+        Name of the NCBI protein database (e.g., "nr").
+    e_value (float): 
+        E-value threshold for significant alignments.
+    min_identity (float): 
+        Minimum percent sequence identity for hits.
 
     Methods
     -------
     init:
         Initialize
+    blast_to_go:
+        ...
     web_blast:
+        ...
+    get_go_from_uniprot:
         ...
     """
 
@@ -29,29 +40,114 @@ class BlastToGo:
         self,
         e_value: float = 0.001,
         program: str = "blastp",
-        min_identity: int = 95,
+        min_identity: float = 95.0,
         database: str = "nr",
+        alignments: int = 10,
+        verbose: bool = False
     ) -> None:
+        """__init__
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        None
+        """
         self.e_value = e_value
         self.min_identity = min_identity
         self.database = database
         self.program = program
+        self.alignments = alignments
+        self.verbose = verbose
+        self.uniprot_url = "https://rest.uniprot.org/uniprotkb/search"
+
+    def blast_to_go(self, seq: str) -> list[str]:
+        """blast_to_go
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        List of protein ids
+        """
+        pids = self.web_blast(seq)
+        go_ids = self.get_go_from_uniprot(pids)
+        return go_ids
 
     @typechecked
-    def web_blast(self, seq) -> str:
+    def web_blast(self, seq: str) -> list[str]:
         """web_blast
 
-        Returns:
-        protein sequence with significant alignments.
+        Parameters
+        ----------
+
+        Returns
+        -------
+        List of protein ids
         """
+        Entrez.email = "briano@bioteam.net"
+        Entrez.NCBI_API_KEY = os.getenv('NCBI_API_KEY')
+
+        pids = list()
         # Run BLAST search using NCBIWWW
         blast_results = NCBIWWW.qblast(
-            query=seq, program=self.program, database=self.database, expect=self.e_value
+            self.program,
+            self.database,
+            seq,
+            expect=self.e_value,
+            alignments=self.alignments,
         )
         blast_record = NCBIXML.read(blast_results)
 
         for alignment in blast_record.alignments:
             for hsp in alignment.hsps:
-                # Skip 100% identity, could be same protein and species
-                if hsp.identities / len(hsp.query) * 100 >= self.min_identity:
-                    return hsp.id
+                percent_identity = float(hsp.identities / len(hsp.query) * 100)
+                # Skip 100% identity, could be the same protein
+                if percent_identity < 100 and percent_identity >= self.min_identity:
+                    # ref|WP_021461111.1|
+                    pid = alignment.hit_id.split("|")[1]
+                    pids.append(pid)
+                    if self.verbose:
+                        logging.debug(f"alignment.hit_id: {pid}")
+        if len(pids) > 0:
+            return pids
+
+    @typechecked
+    def get_go_from_uniprot(self, pids: list[str]) -> list[str]:
+        """get_go_from_uniprot
+
+        https://rest.uniprot.org/uniprotkb/search?query=WP_021461111
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        List of GO ids
+        """
+        for pid in pids:
+            response = requests.get(self.uniprot_url, params={"query": pid})
+            result = json.loads(response.text)["results"]
+            # UniProt may not have the id provided by NCBI Blast
+            if len(result) > 0:
+                """
+                >>> result[0]["uniProtKBCrossReferences"][4]
+                {'database': 'GO', 'id': 'GO:0005737', 'properties':
+                [{'key': 'GoTerm', 'value': 'C:cytoplasm'},
+                {'key': 'GoEvidenceType', 'value': 'IEA:UniProtKB-SubCell'}]}
+                """
+                go_ids = [
+                    x["id"]
+                    for x in result[0]["uniProtKBCrossReferences"]
+                    if x["database"] == "GO"
+                ]
+                if self.verbose:
+                    logging.debug(
+                        f"NCBI BLAST id: {pid} UniProt primary accession: {result[0]['primaryAccession']} GO terms: {go_ids}"
+                    )
+                return go_ids
+            else:
+                if self.verbose:
+                    logging.debug(f"No match in UniProt for NCBI Blast id {pid}")
--- test/test_blast_to_go.py
+++ test/test_blast_to_go.py
@@ -1,54 +1,20 @@
-import argparse
-import json
-import requests
-from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
+import sys
+from pathlib import Path
+
+file = Path(__file__).resolve()
+package_root_directory = file.parents[1]
+sys.path.append(str(package_root_directory))
+
+from protcast.model.stats.blast_to_go import BlastToGo  # noqa: E402
 
 
 if __name__ == "__main__":
     """test_blast_to_go.py
-    Run NCBI blast, get GO terms from best, non-identical match using Uniprot API
+    Run BlastToGo, which gets GO terms from best, non-identical match using Uniprot API
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-e", "--evalue", type=float, default=0.001)
-    parser.add_argument("-p", "--program", default="blastp")
-    parser.add_argument("-m", "--min_identity", type=float, default=95.0)
-    parser.add_argument("-d", "--database", default="nr")
-    args = parser.parse_args()
 
     seq = "MADTFKEIDAQNAWQLVQERQAFLVDVRDIQRFAYSHPQAAFHLTNQSYGEFCQRCDFEDPIVVICYHGNSSRNVAQFLVEQGFDEVYSVRGGFDAWCKAELPLEQGL"
 
-    uniprot_url = "https://rest.uniprot.org/uniprotkb/search"
-
-    blast_results = NCBIWWW.qblast(args.program, args.database, seq, expect=args.evalue)
-    blast_record = NCBIXML.read(blast_results)
-
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            percent_identity = float(hsp.identities / len(hsp.query) * 100)
-            # Skip 100% identity, could be the same protein
-            if percent_identity < 100 and percent_identity >= args.min_identity:
-                # ref|WP_021461111.1|
-                pid = alignment.hit_id.split("|")[1]
-                # https://rest.uniprot.org/uniprotkb/search?query=WP_021461111
-                response = requests.get(uniprot_url, params={"query": pid})
-                result = json.loads(response.text)["results"]
-                # UniProt may not have the id used by NCBI
-                if len(result) > 0:
-                    """
-                    >>> result[0]["uniProtKBCrossReferences"][4]
-                    {'database': 'GO', 'id': 'GO:0005737', 'properties':
-                    [{'key': 'GoTerm', 'value': 'C:cytoplasm'},
-                    {'key': 'GoEvidenceType', 'value': 'IEA:UniProtKB-SubCell'}]}
-                    """
-                    go_ids = [
-                        x["id"]
-                        for x in result[0]["uniProtKBCrossReferences"]
-                        if x["database"] == "GO"
-                    ]
-                    acc = result[0]["primaryAccession"]
-                    print(
-                        f"NCBI id: {pid} UniProt primary accession: {acc} GO terms: {go_ids}"
-                    )
-                else:
-                    print(f"No match in UniProt for {pid}")
+    app = BlastToGo(verbose=True)
+    go_ids = app.blast_to_go(seq)
+    assert len(go_ids) == 3
