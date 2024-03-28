359c869aadcf82234d954f2bc622b1feb73faba2
Brian Osborne <bosborne@alum.mit.edu>
2024-03-28 14:07:54 -0400

    In progress

--- protcast/model/binary_classifier.py
+++ protcast/model/binary_classifier.py
@@ -61,79 +61,82 @@ class BinaryClassifier:
         self.fraction = fraction
         self.neurons = neurons
         self.dropout = dropout
+        self.column_names = list()
 
     @typechecked
     def run(self) -> None:
+        self.get_feature_vectors()
         self.make_featurespace()
-        train_dataset, val_dataset = self.prepare_data()
-        self.make_model(train_dataset, val_dataset)
+        train_tfdataset, val_tfdataset = self.prepare_data()
+        self.make_model(train_tfdataset, val_tfdataset)
+
+    @typechecked
+    def get_feature_vectors(self) -> None:
+        # Get feature vectors for all proteins as a list of lists
+        self.target_features, target_ids = get_ifeatpro_features(
+            self.algorithm, self.target_seqs
+        )
+        self.non_target_features, non_target_ids = get_ifeatpro_features(
+            self.algorithm, self.non_target_seqs
+        )
+        self.all_ids = target_ids + non_target_ids
 
     @typechecked
     def make_featurespace(self) -> None:
-        # Get feature vectors for all proteins as a list of lists
-        target_features, target_ids = get_ifeatpro_features(
-            self.algorithm, self.target_seqs
-        )
-        non_target_features, non_target_ids = get_ifeatpro_features(
-            self.algorithm, self.non_target_seqs
-        )
         # Set up the size and type (float) in the FeatureSpace object and get the column names
         features = dict()
-        self.column_names = list()
-        for count in range(len(target_features[0])):
+        for count in range(len(self.target_features[0])):
             features[str(count)] = FeatureSpace.float_normalized()
             self.column_names.append(str(count))
         self.feature_space = FeatureSpace(features=features)
 
         # Add target values of 0 or 1 to data
-        target_features = [x + [1] for x in target_features]
-        non_target_features = [x + [0] for x in non_target_features]
+        self.target_features = [x + [1] for x in self.target_features]
+        self.non_target_features = [x + [0] for x in self.non_target_features]
+        self.all_features = self.target_features + self.non_target_features
 
-        # Add "target" column name to data
+        # Add "target" column name
         self.column_names.append("target")
-
-        self.all_features = target_features + non_target_features
-        self.all_ids = target_ids + non_target_ids
 
     @typechecked
     def prepare_data(self) -> tuple:
         all_dataframe = pd.DataFrame(self.all_features, columns=self.column_names)
         self.val_dataframe = all_dataframe.sample(frac=self.fraction, random_state=1337)
         self.train_dataframe = all_dataframe.drop(self.val_dataframe.index)
-        train_ds = self.dataframe_to_tfdataset(self.train_dataframe)
-        val_ds = self.dataframe_to_tfdataset(self.val_dataframe)
+        train_tfds = self.dataframe_to_tfdataset(self.train_dataframe)
+        val_tfds = self.dataframe_to_tfdataset(self.val_dataframe)
 
         # why batched into 32?
-        train_ds = train_ds.batch(32)
-        val_ds = val_ds.batch(32)
+        train_tfds = train_tfds.batch(32)
+        val_tfds = val_tfds.batch(32)
 
         # The function adapt() that adapts the Featurespace to the training data only works on
         # datasets dicts of feature values so we have to make a version of the dataset with the labels stripped
-        train_ds_with_no_labels = train_ds.map(lambda x, _: x)
+        train_tfds_no_labels = train_tfds.map(lambda x, _: x)
         # train_ds_with_no_labels = [x for x, _ in train_ds]
 
         # adapt() is kind of magical. During this time the FeatureSpace will:
         # Index the set of possible values for the categorical features, compute mean and variance to aid with
         # normalizing the numerical features plus compute the value boundaries for the different bins for
         # numerical features to discretize.
-        self.feature_space.adapt(train_ds_with_no_labels)
+        self.feature_space.adapt(train_tfds_no_labels)
 
         # Attempt at asynch preprocessing not sure if CLAB hardware is optimized for this yet though
         # Running it as part of the tf.data pipeline instead of the model itself
-        preprocessed_train_ds = train_ds.map(
+        processed_train_tfds = train_tfds.map(
             lambda x, y: (self.feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
         )
-        preprocessed_train_ds = preprocessed_train_ds.prefetch(tf.data.AUTOTUNE)
+        processed_train_tfds = processed_train_tfds.prefetch(tf.data.AUTOTUNE)
 
-        preprocessed_val_ds = val_ds.map(
+        processed_val_tfds = val_tfds.map(
             lambda x, y: (self.feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
         )
-        preprocessed_val_ds = preprocessed_val_ds.prefetch(tf.data.AUTOTUNE)
+        processed_val_tfds = processed_val_tfds.prefetch(tf.data.AUTOTUNE)
 
-        return preprocessed_train_ds, preprocessed_val_ds
+        return processed_train_tfds, processed_val_tfds
 
     @typechecked
-    def make_model(self, train_ds: tf.data.Dataset, val_ds: tf.data.Dataset) -> None:
+    def make_model(self, train_tfds: tf.data.Dataset, val_tfds: tf.data.Dataset) -> None:
         encoded_features = self.feature_space.get_encoded_features()
         # Create a dense layer with 32 neurons and apply the ReLU activation function to
         # the data received from encoded_features.
@@ -156,9 +159,9 @@ class BinaryClassifier:
         )
         # Here's a pipeline model that will be trained and called seperately
         self.training_model.fit(
-            train_ds,
+            train_tfds,
             epochs=self.epochs,
-            validation_data=val_ds,
+            validation_data=val_tfds,
         )
 
     @typechecked
@@ -171,9 +174,9 @@ class BinaryClassifier:
                     type = f"non-{self.name}"
                 # Pre-process the sample you want a prediction from
                 del r["target"]
-                sample_ds = self.sample_preprocessing(r)
+                sample_tfds = self.sample_preprocessing(r)
                 # Get a prediction
-                predictions = self.training_model.predict(sample_ds)
+                predictions = self.training_model.predict(sample_tfds)
                 f.write(f"{type}\t{self.all_ids[i]}\t{100 * predictions[0][0]:.2f}\n")
 
     @typechecked
@@ -190,14 +193,14 @@ class BinaryClassifier:
         # Convert pandas Series into dataframe
         sample_frame = pd.DataFrame([sample])
         # Convert datafrane into Tensorflow Datasest with stub target
-        sample_ds = tf.data.Dataset.from_tensor_slices((dict(sample_frame), [0]))
+        sample_tfds = tf.data.Dataset.from_tensor_slices((dict(sample_frame), [0]))
         # Batch of 1 since there's only 1 sample
-        sample_ds = sample_ds.batch(1)
+        sample_tfds = sample_tfds.batch(1)
         # Pre-process the dataset using the FeatureSpace map
-        preprocessed_sample_ds = sample_ds.map(
+        processed_sample_tfds = sample_tfds.map(
             lambda x, y: (self.feature_space(x), y), num_parallel_calls=tf.data.AUTOTUNE
         )
-        return preprocessed_sample_ds
+        return processed_sample_tfds
 
     @typechecked
     def save_model(self) -> None:
