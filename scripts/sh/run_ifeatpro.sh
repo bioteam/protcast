5797f6f00cd32c72e59c48318d7f5a6056531fb5
Brian Osborne <zbio.oibz@gmail.com>
2024-05-02 14:16:35 -0500

    Correct algorithm names

--- scripts/sh/run_ifeatpro.sh
+++ scripts/sh/run_ifeatpro.sh
@@ -9,9 +9,8 @@
 
 module load all/TensorFlow/2.15.1-Python-3.10
 
-#algorithms=(aac apaac cksaagp cksaap ctdc ctdd ctdt ctriad de dpc gaac gdpc geary gtpc ksctriad moran nmbroto paac qsorder socnumber tpc)
-
-algorithms=(ctriad moran nmbroto paac qsorder socnumber tpc)
+#algorithms=(aac apaac cksaagp cksaap ctdc ctdd ctdt ctriad dde dpc gaac gdpc geary gtpc ksctriad moran nmbroto paac qsorder socnumber tpc)
+algorithms=(cksaap dde tpc)
 
 for alg in "${algorithms[@]}"; do
     python3 scripts/binary_classify.py -n gpcrs -t test/data/uniprotkb_gpcrs.fasta -nt test/data/uniprotkb_non-gpcrs.fasta -a "$alg"
