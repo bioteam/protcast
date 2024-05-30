f54627754106a5346394f3d5befa476bf7b8d60d
Brian Osborne <bosborne@alum.mit.edu>
2024-05-30 14:57:19 -0400

    Minor doc edits

--- scripts/test_cuda.py
+++ scripts/test_cuda.py
@@ -4,26 +4,27 @@ import numpy as np
 # to measure exec time
 from timeit import default_timer as timer
 
+num = 10000000
+
 
 # normal function to run on cpu
 def func(a):
-    for i in range(10000000):
+    for i in range(num):
         a[i] += 1
 
 
 # function optimized to run on gpu
 @jit(target_backend="cuda")
 def func2(a):
-    for i in range(10000000):
+    for i in range(num):
         a[i] += 1
 
 
 if __name__ == "__main__":
-    n = 10000000
-    a = np.ones(n, dtype=np.float64)
+    a = np.ones(num, dtype=np.float64)
     start = timer()
     func(a)
-    print("without GPU:", timer() - start)
+    print("Without GPU:", timer() - start)
     start = timer()
     func2(a)
-    print("with GPU:", timer() - start)
+    print("With GPU:", timer() - start)
