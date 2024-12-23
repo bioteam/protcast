fde900b80771dbd8ae5b925f773aa10e29f35a32
Brian Osborne <bosborne@alum.mit.edu>
2024-12-23 13:04:54 -0500

    Attempt fix

--- scripts/create_subgraph_sequences_files.py
+++ scripts/create_subgraph_sequences_files.py
@@ -53,12 +53,16 @@ for go_id in go_ids:
     target_go_ids = dataset.get_subgraph(go_id)
     go_terms = dataset.get_terms(target_go_ids)
     annots = [go_term.get_all_annotations for go_term in go_terms]
-    target_seq_ids = [annot.protein_id for annot in annots]
+    target_seq_ids = [
+        annot.protein_id for annot in [x for y in annots for x in y]
+    ]
 
     non_target_go_ids = dataset.get_inverse_subgraph(go_id)
     go_terms = dataset.get_terms(non_target_go_ids)
     annots = [go_term.get_all_annotations for go_term in go_terms]
-    non_target_seq_ids = [annot.protein_id for annot in annots]
+    non_target_seq_ids = [
+        annot.protein_id for annot in [x for y in annots for x in y]
+    ]
 
     random_non_target_seq_ids = [
         random.choice(non_target_go_ids) for x in range(len(target_seq_ids))
