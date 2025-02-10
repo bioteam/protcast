42f004d48382fed76b6778c72dc02ba961aaa938
Brian Osborne <bosborne@alum.mit.edu>
2025-02-10 13:38:10 -0500

    Fixes for feature vectors

--- scripts/make_multi_class_model.py
+++ scripts/make_multi_class_model.py
@@ -12,80 +12,78 @@ from protcast.preprocessing.protcast_dataset import (  # noqa: E402
     ProtCastDataset,
 )
 
-if __name__ == "__main__":
-    """"make_multi_class_model.py
-    Provide a Fasta file, or a text file with GO ids and a ProtCastDataset file. 
-    Example to use a ProtCastDataset:
+""""make_multi_class_model.py
+Provide a Fasta file, or a text file with GO ids and a ProtCastDataset file. 
+Example to use a ProtCastDataset:
 
-    python3 scripts/make_multi_class_model.py \
-    -g test/data/go-terms.txt \
-    -p ProtcastDataset.bin \
-    -a qsorder -f ifeatpro -s
+python3 scripts/make_multi_class_model.py \
+-g test/data/go-terms.txt \
+-p ProtcastDataset.bin \
+-a qsorder
 
-    Or use a sequence (Fasta) file:
+Or use a sequence (Fasta) file:
 
-    python3 scripts/make_multi_class_model.py \
-    -s test/data/random-level-4.fa \
-    -v
+python3 scripts/make_multi_class_model.py \
+-s test/data/random-level-4.fa \
+-v
+"""
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--go_ids_file", help="Path to GO ids file")
+parser.add_argument("-s", "--seq_file", help="Path to Fasta file")
+parser.add_argument(
+    "-a", "--algorithm", default="CTriad", help="Feature vector algorithm"
+)
+parser.add_argument(
+    "-p",
+    "--protcast_dataset",
+    help="Path to ProtCast dataset",
+)
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose")
+args = parser.parse_args()
 
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--go_ids_file", help="Path to GO ids file")
-    parser.add_argument("-s", "--seq_file", help="Path to Fasta file")
-    parser.add_argument(
-        "-a", "--algorithm", default="CTriad", help="Feature vector algorithm"
-    )
-    parser.add_argument(
-        "-p",
-        "--protcast_dataset",
-        help="Path to ProtCast dataset",
-    )
-    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose")
-    args = parser.parse_args()
+start = time.time()
 
-    start = time.time()
+# Primary keys are GO ids, secondary keys are protein ids, values are sequences
+proteins = defaultdict(dict)
 
-    # Primary keys are GO ids, secondary keys are protein ids, values are sequences
-    proteins = defaultdict(dict)
+# GO id subgraph sequences are collected from a ProtCastDataset
+if args.go_ids_file is not None and args.protcast_dataset is not None:
+    dataset = ProtCastDataset.load_serialized_file(args.protcast_dataset)
+    go_ids = [
+        line.strip()
+        for line in open(args.go_ids_file, "r")
+        if line.startswith("GO:")
+    ]
+    for go_id in go_ids:
+        subgraph_go_ids = dataset.get_subgraph(go_id)
+        for subid in subgraph_go_ids:
+            pids = dataset.get_term(subid).get_all_pids()
+            if pids:
+                seqs = {
+                    pid: dataset.proteins[pid].sequence
+                    for pid in pids
+                    if pid in dataset.proteins
+                }
+                proteins[go_id].update(seqs)
+# Sequences are collected from a fasta file where the description contains a GO id
+elif args.seq_file is not None:
+    from Bio import SeqIO
 
-    # GO id subgraph sequences are collected from a ProtCastDataset
-    if args.go_ids_file is not None and args.protcast_dataset is not None:
-        dataset = ProtCastDataset.load_serialized_file(args.protcast_dataset)
-        go_ids = [
-            line.strip()
-            for line in open(args.go_ids_file, "r")
-            if line.startswith("GO:")
-        ]
-        for go_id in go_ids:
-            subgraph_go_ids = dataset.get_subgraph(go_id)
-            for subid in subgraph_go_ids:
-                pids = dataset.get_term(subid).get_all_pids()
-                if pids:
-                    seqs = {
-                        pid: dataset.proteins[pid].sequence
-                        for pid in pids
-                        if pid in dataset.proteins
-                    }
-                    proteins[go_id].update(seqs)
-    # Sequences are collected from a fasta file where the description contains a GO id
-    elif args.seq_file is not None:
-        from Bio import SeqIO
+    for seq in SeqIO.parse(args.seq_file, "fasta"):
+        go_id = re.search("GO:\d+", seq.description)[0]
+        if go_id is not None:
+            proteins[go_id][seq.id] = str(seq.seq)
+else:
+    sys.exit("Need input files")
 
-        for seq in SeqIO.parse(args.seq_file, "fasta"):
-            go_id = re.search("GO:\d+", seq.description)[0]
-            if go_id is not None:
-                proteins[go_id][seq.id] = str(seq.seq)
-    else:
-        sys.exit("Need input files")
-
-    classifier = MultiClassifier(
-        args.algorithm,
-        args.verbose,
-        proteins,
-    )
-    classifier.run()
-    # Not necessary with the checkpoints in place
-    # classifier.save_model()
+classifier = MultiClassifier(
+    args.algorithm,
+    args.verbose,
+    proteins,
+)
+classifier.run()
+# Not necessary with the checkpoints in place
+# classifier.save_model()
 
 end = time.time()
 print(f"Elapsed {args.algorithm} training time: {round(end - start)}")
--- scripts/run_multi_class_inference.py
+++ scripts/run_multi_class_inference.py
@@ -44,7 +44,7 @@ algorithm = re.search(
     r"\d+-\d+-\d+-\d+-\d+-\d+_(.+)\.keras$", args.model_file
 )[1]
 go_encoder = GOEncoder.load(args.goencoder_file)
-fv = Calculator()
+fv_factory = Calculator()
 
 # Collect data for F1 score calculation
 true = list()
@@ -53,14 +53,11 @@ names = dict()
 
 print("Protein\tActual GO\tPredicted GO\tProbability\tName")
 for seq in SeqIO.parse(args.seq_file, "fasta"):
-    fv = fv_factory.get_feature_vectors({seq.id: str(seq.seq)})
-    # A feature vector may not be created if the sequence is too short
-    if len(fv[0]) == 0:
-        continue
+    fv_factory.get_feature_vectors(algorithm, pdict={seq.id: str(seq.seq)})
     # The feature vector is a 1D array, but the model expects a 2D array where the first dimension
     # is the batch size (None means it can be any number), and the second dimension is the length
     # of the feature vector. -1 in the reshape() tells numpy to infer the second dimension.
-    X_test = np.array(fv[0][0]).reshape(1, -1)
+    X_test = np.array(fv_factory.encodings[0][0]).reshape(1, -1)
     y_pred_ps = model.predict(X_test)
     pred_tup = go_encoder.decode_probabilities(y_pred_ps, top_k=1)[0][0]
     # Get actual GO and name from the sequence description
