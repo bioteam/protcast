0ac23779b1de9519a350ffada0f63e0b457b55b7
Brian Osborne <bosborne@alum.mit.edu>
2026-01-19 17:44:50 -0500

    Add file loop to job script

--- scripts/sh/run_make_multi_class_model_embeds.sh
+++ scripts/sh/run_make_multi_class_model_embeds.sh
@@ -9,17 +9,19 @@
 
 CONTAINER=${WORK}/tensorflow_2.17.0-gpu.sif
 DATADIR=${WORK}/ProtCast/ProtCastDataset/11-03-2025
-EMBEDDIR=mf_go_terms-level-3
+EMBEDDIR=mf_go_terms-level-
 
-# Only use modules from the container with Python 3.11
+# Only use local modules for Python 3.11 to match the Python version in the container
 export PYTHONPATH=$HOME/.local/lib/python3.11/site-packages
 module load tacc-apptainer
 
 cd ${HOME}/git/ProtCast/
 
-singularity exec --nv $CONTAINER \
-python3 scripts/make_multi_class_model_embeds.py \
--v \
--p $DATADIR/ProtCastDataset.bin \
---input_source esm_embeddings \
--d $DATADIR/$EMBEDDIR
+for LEVEL in 5 6 7 8 9; do
+    singularity exec --nv $CONTAINER \
+    python3 scripts/make_multi_class_model_embeds.py \
+    -v \
+    -p $DATADIR/ProtCastDataset.bin \
+    --input_source esm_embeddings \
+    -d $DATADIR/$EMBEDDIR-${LEVEL}
+done
