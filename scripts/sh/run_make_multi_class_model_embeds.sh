97ed7814d7869fc7e0903503fe6fb0984c253708
Brian Osborne <bosborne@c100-002.frontera.tacc.utexas.edu>
2026-01-19 16:37:01 -0600

    Edits to embed jobs script

--- scripts/sh/run_make_multi_class_model_embeds.sh
+++ scripts/sh/run_make_multi_class_model_embeds.sh
@@ -2,23 +2,24 @@
 #SBATCH --job-name run_make_multi_class_model_embeds
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=briano@bioteam.net
-#SBATCH -o run_make_multi_class_model_embds.out
+#SBATCH -o run_make_multi_class_model_embeds.out
 #SBATCH -e run_make_multi_class_model_embeds.err
 #SBATCH -N 2
 #SBATCH -n 32
 
-CONTAINER=${HOME}/tensorflow_2.17.0-gpu.sif
+CONTAINER=${WORK}/tensorflow_2.17.0-gpu.sif
 DATADIR=${WORK}/ProtCast/ProtCastDataset/11-03-2025
+EMBEDDIR=mf_go_terms-level-3
 
-source ${HOME}/.bash_profile
-# Only use modules from the container
-unset PYTHONPATH
+# Only use modules from the container with Python 3.11
+export PYTHONPATH=$HOME/.local/lib/python3.11/site-packages
 module load tacc-apptainer
 
 cd ${HOME}/git/ProtCast/
 
 singularity exec --nv $CONTAINER \
-python3 scripts/make_multi_class_model.py \
+python3 scripts/make_multi_class_model_embeds.py \
 -v \
 -p $DATADIR/ProtCastDataset.bin \
--d $DATADIR/mf_go_terms-level-4
+--input_source esm_embeddings \
+-d $DATADIR/$EMBEDDIR
