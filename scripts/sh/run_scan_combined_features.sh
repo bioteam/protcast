2d82ae25848e252d4be5b660150dbf1e751ba6cd
Brian Osborne <bosborne@alum.mit.edu>
2026-06-13 23:16:41 -0400

    New version of shell script that loops over levels

--- scripts/sh/run_scan_combined_features.sh
+++ scripts/sh/run_scan_combined_features.sh
@@ -10,9 +10,7 @@
 CONTAINER=${WORK}/tensorflow_2.17.0-gpu.sif
 DATADIR=${WORK}/ProtCast/ProtCastDataset/01-23-2026
 EMBEDDIR=mf_go_terms-level
-LEVEL=8
 SEED=22
-OUTDIR=${WORK}/ProtCast/feature_scan_combined-level-${LEVEL}-seed-${SEED}-mean_max_std
 
 # Only use local modules for Python 3.11 to match the Python version in the container
 export PYTHONPATH=$HOME/.local/lib/python3.11/site-packages
@@ -20,21 +18,25 @@ module load tacc-apptainer
 
 cd ${HOME}/git/ProtCast/
 
-echo "============================================"
-echo "Running combined feature scan for GO level ${LEVEL}"
-echo "============================================"
-singularity exec --nv $CONTAINER \
-python3 scripts/scan_individual_features.py \
--v \
--p $DATADIR/ProtCastDataset.bin \
--d $DATADIR/$EMBEDDIR-${LEVEL}-mean_max_std \
--o $OUTDIR \
---seed $SEED \
---algorithms \
-    PseKRAAC_type_3B \
-    PseKRAAC_type_8 \
-    PseKRAAC_type_7 \
-    PseKRAAC_type_3B+PseKRAAC_type_8 \
-    PseKRAAC_type_3B+PseKRAAC_type_7 \
-    PseKRAAC_type_8+PseKRAAC_type_7 \
-    PseKRAAC_type_3B+PseKRAAC_type_8+PseKRAAC_type_7
+for LEVEL in 5 6 7 8; do
+    OUTDIR=${WORK}/ProtCast/feature_scan_combined-level-${LEVEL}-seed-${SEED}-mean_max_std
+
+    echo "============================================"
+    echo "Running combined feature scan for GO level ${LEVEL}"
+    echo "============================================"
+    singularity exec --nv $CONTAINER \
+    python3 scripts/scan_individual_features.py \
+    -v \
+    -p $DATADIR/ProtCastDataset.bin \
+    -d $DATADIR/$EMBEDDIR-${LEVEL}-mean_max_std \
+    -o $OUTDIR \
+    --seed $SEED \
+    --algorithms \
+        PseKRAAC_type_3B \
+        PseKRAAC_type_8 \
+        PseKRAAC_type_7 \
+        PseKRAAC_type_3B+PseKRAAC_type_8 \
+        PseKRAAC_type_3B+PseKRAAC_type_7 \
+        PseKRAAC_type_8+PseKRAAC_type_7 \
+        PseKRAAC_type_3B+PseKRAAC_type_8+PseKRAAC_type_7
+done
